b2a1879113c63d43bbaba219468693a9af894cbe
Nikita Karamov <me@kytta.dev>
2024-10-22 18:24:22 +0200

    Make implementation more httpx-like
    - rename `session` to `client`
    - use httpx-native proxy mounts
    - use httpx.Response.json()

--- geopy/adapters.py
+++ geopy/adapters.py
@@ -689,49 +689,100 @@ class RequestsHTTPWithSSLContextAdapter(RequestsHTTPAdapter):
             conn.key_file = None
 
 
-class AsyncHttpxAdapter(BaseAsyncAdapter):
+class HttpxAsyncAdapter(BaseAsyncAdapter):
+    """The adapter which uses `httpx`_ library.
+
+    .. _httpx: https://www.python-httpx.org/
+
+    `httpx` supports keep-alives, persists Cookies, allows response
+    compression and uses HTTP/2, when installed alongside ``h2``.
+
+    ``httpx`` package must be installed in order to use this adapter.
+    """
     is_available = httpx_available
 
-    def __init__(self, *, proxies, ssl_context):
+    def __init__(
+        self,
+        *,
+        proxies,
+        ssl_context,
+        max_keepalive_connections=20,
+        max_connections=100,
+        keepalive_expiry=5,
+    ):
         if not httpx_available:
             raise ImportError(
-                "`httpx` must be installed in order to use AsyncHttpxAdapter. "
+                "`httpx` must be installed in order to use HttpxAsyncAdapter. "
                 "If you have installed geopy via pip, you may use "
                 "this command to install httpx: "
                 '`pip install httpx`.'
             )
         proxies = _normalize_proxies(proxies)
+        if ssl_context is None:
+            # Like requests, httpx uses CA bundle from `certifi` package.
+            # This is typically overridden with the `SSL_CERT_FILE`
+            # or `SSL_CERT_DIR` environment variable. However, trust_env
+            # is disabled below to turn off the httpx-specific logic of proxy
+            # servers configuration, which is re-implemented in geopy
+            # so that it's similar between different Adapters implementations.
+            #
+            # Here, in order to align the adapter's behavior with
+            # the default URLLibAdapter, we explicitly pass an ssl context,
+            # which would be initialized with the system's CA store
+            # rather than the certifi's bundle requests uses by default.
+            #
+            # See also https://github.com/geopy/geopy/issues/546
+            ssl_context = ssl.create_default_context()
         super().__init__(proxies=proxies, ssl_context=ssl_context)
 
-        self.proxies = proxies
+        limits = httpx.Limits(
+            max_keepalive_connections=max_keepalive_connections,
+            max_connections=max_connections,
+            keepalive_expiry=keepalive_expiry,
+        )
+
+        self._proxy_mounts = {
+            ("%s://" % scheme): httpx.AsyncHTTPTransport(
+                verify=ssl_context if scheme == "https" else False,
+                proxy=proxy,
+                limits=limits,
+            )
+            for scheme, proxy
+            in proxies.items()
+        }
         self.ssl_context = ssl_context
 
     @property
-    def session(self):
-        # Lazy session creation, which allows to avoid "unclosed socket"
+    def client(self):
+        # Lazy client creation, which allows to avoid "unclosed socket"
         # warnings if a Geocoder instance is created without entering
         # async context and making any requests.
-        session = self.__dict__.get("session")
-        if session is None:
-            session = httpx.AsyncClient(
+        client = self.__dict__.get("client")
+        if client is None:
+            client = httpx.AsyncClient(
                 trust_env=False,  # don't use system proxies
+                mounts=self._proxy_mounts,
             )
-            self.__dict__["session"] = session
-        return session
+            self.__dict__["client"] = client
+        return client
 
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.session.aclose()
+        await self.client.aclose()
 
     async def get_text(self, url, *, timeout, headers):
-        response = await self.session.get(
+        response = await self._request(url, timeout=timeout, headers=headers)
+        return response.text
+
+    async def get_json(self, url, *, timeout, headers):
+        response = await self._request(url, timeout=timeout, headers=headers)
+        return response.json()
+
+    async def _request(self, url, *, timeout, headers):
+        return await self.client.get(
             url,
             timeout=httpx.Timeout(timeout),
             headers=headers
         )
-        return response.text
-
-    async def get_json(self, *args, **kwargs):
-        return json.loads(await self.get_text(*args, **kwargs))
