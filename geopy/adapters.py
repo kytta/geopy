442aa3bedfaecbcd7ba364631e3f592658a6b078
ae <85891169+novitae@users.noreply.github.com>
2024-10-22 17:57:11 +0200

    Basic implementation of the HttpxAdapter

--- geopy/adapters.py
+++ geopy/adapters.py
@@ -61,6 +61,12 @@ try:
 except ImportError:
     aiohttp_available = False
 
+try:
+    import httpx
+    httpx_available = True
+except ImportError:
+    httpx_available = False
+
 
 class AdapterHTTPError(IOError):
     """An exception which must be raised by adapters when an HTTP response
@@ -681,3 +687,51 @@ class RequestsHTTPWithSSLContextAdapter(RequestsHTTPAdapter):
             conn.ca_cert_dir = None
             conn.cert_file = None
             conn.key_file = None
+
+
+class AsyncHttpxAdapter(BaseAsyncAdapter):
+    is_available = httpx_available
+
+    def __init__(self, *, proxies, ssl_context):
+        if not httpx_available:
+            raise ImportError(
+                "`httpx` must be installed in order to use AsyncHttpxAdapter. "
+                "If you have installed geopy via pip, you may use "
+                "this command to install httpx: "
+                '`pip install httpx`.'
+            )
+        proxies = _normalize_proxies(proxies)
+        super().__init__(proxies=proxies, ssl_context=ssl_context)
+
+        self.proxies = proxies
+        self.ssl_context = ssl_context
+
+    @property
+    def session(self):
+        # Lazy session creation, which allows to avoid "unclosed socket"
+        # warnings if a Geocoder instance is created without entering
+        # async context and making any requests.
+        session = self.__dict__.get("session")
+        if session is None:
+            session = httpx.AsyncClient(
+                trust_env=False,  # don't use system proxies
+            )
+            self.__dict__["session"] = session
+        return session
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.aclose()
+
+    async def get_text(self, url, *, timeout, headers):
+        response = await self.session.get(
+            url,
+            timeout=httpx.Timeout(timeout),
+            headers=headers
+        )
+        return response.text
+
+    async def get_json(self, *args, **kwargs):
+        return json.loads(await self.get_text(*args, **kwargs))
